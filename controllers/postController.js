e7b6dc12c0e24afdd36a28c290ed1f873818e44b
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-12-06 16:11:24 +0100

    add bonus show tags

--- controllers/postController.js
+++ controllers/postController.js
@@ -27,6 +27,8 @@ function index(req, res) {
 
     const sql = 'SELECT * FROM posts';
 
+
+
     connection.query(sql, (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
         res.json(results);
@@ -38,11 +40,41 @@ function show(req, res) {
 
     const id = req.params.id
     const sql = 'SELECT * FROM posts WHERE id = ?';
+
+    const tagsSql = `
+    SELECT tags.*
+    FROM tags
+    JOIN post_tag ON tags.id = post_tag.tag_id
+    WHERE post_tag.post_id = ?
+  `;
+
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
         if (results.length === 0) return res.status(404).json({ error: 'Post non trovato' });
-        res.json(results[0]);
-    });
+
+
+        const posts = results[0]
+
+
+        connection.query(tagsSql, [id], (err, tagsResults) => {
+
+            if (err) return res.status(500).json({ error: err })
+
+            posts.tags = tagsResults;
+
+
+            const responseData = {
+                data: posts,
+            }
+
+
+
+            res.status(200).json(responseData);
+
+        })
+
+    })
+
 }
 
 
